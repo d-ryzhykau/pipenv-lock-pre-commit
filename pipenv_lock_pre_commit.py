934c938acced375ea19d2c65af05936a62e75d28
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-03-06 00:42:07 +0100

    Ignore pre-commit virtualenv

--- pipenv_lock_pre_commit.py
+++ pipenv_lock_pre_commit.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+import os
 import subprocess
 import sys
 from argparse import ArgumentParser, FileType
+
+
+def _get_env():
+    env = os.environ.copy()
+    env["PIPENV_IGNORE_VIRTUALENVS"] = "1"
+    return env
 
 
 def requirements():
@@ -17,10 +24,11 @@ def requirements():
     out = subprocess.run(
         ["pipenv", "requirements", *extra_args],
         stdout=args.requirements_file,
+        env=_get_env(),
     )
     return sys.exit(out.returncode)
 
 
 def verify():
-    out = subprocess.run(["pipenv", "verify"])
+    out = subprocess.run(["pipenv", "verify"], env=_get_env())
     sys.exit(out.returncode)
